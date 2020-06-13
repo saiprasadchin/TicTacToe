bb05e39426db921eec03cad6847be11eb8179e2e
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-13 00:10:11 -0700

    Added:Winning function to check winner after every move

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -69,9 +69,58 @@ function playerPlaying() {
         board[$position]=$playerLatter
         switchPlayer=1
 }
+function checkingEmptyCell() {
+        if [[ $position -ge 1 && $position -le 9 ]]
+                then
+                        if [[ ${board[$position]} == . ]]
+                        then
+                        echo "............ $turnChange is placed at $position ............"
+                        ((count++))
+                        else
+                                echo "Cell is already occupied!!!"
+                                switchPlayer
+                        fi
+         else
+                 echo "Invalid cell value!!!"
+                 switchPlayer
+         fi
+}
+function winningCondition() {
+        for((i=1;i<=$TOTAL_CELL;i=$(($i+3))))
+        do
+                if [[ ${board[$i]} == ${board[$i+1]} && ${board[$i+1]} == ${board[$i+2]} && ${board[$i+2]} == $1 ]]
+                then
+                        winner=1
+                fi
+        done
+        for((i=1;i<=3;i++))
+        do
+                if [[ ${board[$i]} == ${board[$i+3]} && ${board[$i+3]} == ${board[$i+6]} && ${board[$i]} == $1 ]]
+                then
+                        winner=1
+                fi
+        done
+        if [[ ${board[1]}  == ${board[5]} && ${board[5]} == ${board[9]} && ${board[5]} == $1 ]]
+        then
+                winner=1
+        elif [[ ${board[3]}  == ${board[5]} && ${board[5]} == ${board[7]} && ${board[5]} == $1 ]]
+        then
+                winner=1
+        fi
+}
+function checkingGameStatus() {
+        if [[ $winner -eq 1 ]]
+        then
+                echo "Winner is $turnChange's"
+                exit
+        elif [[ $count -ge $TOTAL_CELL ]]
+        then
+                echo tie
+        fi
+}
 resettingBoard
-
 tossToPlay
 displayBoard
 switchPlayer
 displayBoard
+checkingGameStatus
