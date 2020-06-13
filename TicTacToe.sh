ee02aa89f92428d1215346e125b5b91936df7d6b
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-12 23:26:38 -0700

    Added: UC1 Reset Board function

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,19 @@
-#!/bin/bash -x
-echo "Weclome to Tic Tac Toe Game"
+#!/bin/bash
+echo "Weclome to Tic Tac Toe Game."
+#array declaration of array
+declare -a board
+
+function resettingBoard() {
+        board=(. . . . . . . . . .)
+}
+resettingBoard
+function displayBoard() {
+
+        echo "***TicTacToe Board***"
+        echo "|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |"
+        echo "|  ${board[4]}  ||  ${board[5]}  ||  ${board[6]}  |"
+        echo "|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
+        echo "**********************"
+
+}
+displayBoard
